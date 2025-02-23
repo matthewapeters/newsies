65664898d1343d07aa97ca607827425f61517290
MatthewAPeters <matthewapeters@comcast.net>
2025-02-22 22:34:41 -0800

    break out startup routines

---
+++ newsies/cli/main.py
@@ -0,0 +1,27 @@
+from newsies.chroma_client import CRMADB
+from newsies.classify import prompt_analysis
+
+
+def read_news():
+    """
+    read_news:
+      - read news from the database
+    """
+
+    exit_now = False
+    while exit_now == False:
+
+        # Test locally hosted RAG
+        # query = "How does deep learning differ from machine learning and what role does transformer play?"
+        query = input("Question (quit to exit): ").strip()
+        if query == "quit":
+            exit_now = True
+        elif query == "":
+            continue
+        else:
+            # analyze request
+            request_meta = prompt_analysis(query)
+            print(f"(newsies thinks you want to know about {request_meta})")
+            # Generate response using GPT4All
+            response = CRMADB.generate_rag_response(query.lower(), request_meta, llm, 5)
+            print("\nRESPONSE:\n", response)
