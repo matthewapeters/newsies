c0b329040f3a142d57683602f87427b73496ff1f
MatthewAPeters <matthewapeters@comcast.net>
2025-05-02 08:22:14 -0700

    add a util to generate missing embeddings

--- newsies/utils/fix_embeddings.py
+++ newsies/utils/fix_embeddings.py
@@ -5,6 +5,8 @@ import pickle
 from newsies.ap_news.article import Article
 from newsies.ap_news.embedding_visitor import EmbeddingVisitor
 
+# pylint: disable=unused-argument, broad-exception-caught
+
 
 def fix_embeddings():
     """
@@ -15,14 +17,21 @@ def fix_embeddings():
     ) as missing_fh:
         articles = missing_fh.readlines()
     print(f"Found {len(articles)} articles with missing embeddings")
-    for article_id in articles:
-        with open(f"./daily_news/apnews.com/{article_id.strip()}.pkl", "rb") as fh:
-            article: Article = pickle.load(fh)
-
+    try:
+        for article_id in articles:
+            with open(f"./daily_news/apnews.com/{article_id.strip()}.pkl", "rb") as fh:
+                article: Article = pickle.load(fh)
             v = EmbeddingVisitor()
             v.visit(article)
             article.pickle()
             print(f"Fixed {article_id.strip()}")
+        with open(
+            "./daily_news/apnews.com/missing_embeddings.txt", "w", encoding="utf8"
+        ) as missing_fh:
+            missing_fh.write("")
+        print("All articles fixed")
+    except Exception as err:
+        print(f"Error: {err}")
 
 
 if __name__ == "__main__":
